9506ebd72927fa36b960857a6fc0dca8b6843ca4
gonza7795 <gonza_7795@yahoo.com.ar>
2022-05-12 21:48:13 -0300

    probando

---
+++ src/main/java/com/apirest/apirest/model/Estudiante.java
@@ -0,0 +1,80 @@
+
+package com.apirest.apirest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estudiantes")
+
+
+public class Estudiante {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+     private String email;
+     private Double nota;
+
+    public Estudiante() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "id=" + id + ", nombres=" + nombres + ", apellidos=" + apellidos + ", email=" + email + ", nota=" + nota + '}';
+    }
+    
+    
+    
+     
+    
+       
+    
+    
+}
